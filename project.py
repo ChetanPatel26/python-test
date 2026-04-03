2d1b56ebf4db02abc7f76736e9e6e4cb1c0e52bb
ChetanPatel26 <114027015+ChetanPatel26@users.noreply.github.com>
2026-04-03 23:56:28 +0530

    Update project.py

--- project.py
+++ project.py
@@ -13,3 +13,6 @@ print(f"Difference of numbers {a} and {b} is {diff}")
 print("New code added v1.0.5")
 print"Intentionlly putting invalid syntax here")
 
+print("New code added v1.0.6")
+print("Corrected now")
+
