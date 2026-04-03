6dc068691caa8d67601828d95be3d224d30ffd2a
ChetanPatel26 <114027015+ChetanPatel26@users.noreply.github.com>
2026-04-03 20:51:43 +0530

    Correcting last changes, version remains same

--- project.py
+++ project.py
@@ -8,5 +8,5 @@ print("New code added v1.0.2")
 print("New code added v1.0.3")
 print("New code added v1.0.4")
 diff=a-b
-print(f"Sum of numbers {a} and {b} is {diff}")
+print(f"Difference of numbers {a} and {b} is {diff}")
 
