e3c673b28e9558bcf91fb8b0bbd668cf165a4bfe
ChetanPatel26 <114027015+ChetanPatel26@users.noreply.github.com>
2026-04-03 20:50:45 +0530

    Update 4 project.py

--- project.py
+++ project.py
@@ -6,4 +6,7 @@ sum=a+b
 print(f"Sum of numbers {a} and {b} is {sum}")
 print("New code added v1.0.2")
 print("New code added v1.0.3")
+print("New code added v1.0.4")
+diff=a-b
+print(f"Sum of numbers {a} and {b} is {diff}")
 
